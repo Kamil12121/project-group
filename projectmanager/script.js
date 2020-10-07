4f4a46ef3f7228c3d0500d6398ee5a04b087af00
Piwko Marcin <piwkoq@gmail.com>
2020-10-07 13:05:28 +0200

    Refactored and optimalized code

--- projectmanager/script.js
+++ projectmanager/script.js
@@ -1,127 +1,47 @@
-var moverclass = {
-    __init: function () {
-        var elem = document.querySelector('.wrapper'),
-            div = document.querySelector('.move'),
-            x = 0,
-            y = 0,
-            mousedown = false;
-        div.addEventListener('mousedown', function (e) {
-            mousedown = true;
-            x = div.offsetLeft - e.clientX;
-            y = div.offsetTop - e.clientY;
-        }, true);
-        div.addEventListener('mouseup', function (e) {
-            mousedown = false;
-        }, true);
-        elem.addEventListener('mousemove', function (e) {
-            e.preventDefault();
-            if (mousedown) {
-                div.style.left = e.clientX + x + 'px';
-                div.style.top = e.clientY + y + 'px';
-            }
-        }, true);
-    }
-};
-moverclass.__init();
-var moverclass = {
-    __init: function () {
+const wrapper = document.querySelector('.wrapper');
+const wrapper1 = document.querySelector('.wrapper1');
+const wrapper2 = document.querySelector('.wrapper2');
+const wrapper3 = document.querySelector('.wrapper3');
+const div = document.querySelector('.move');
+const div1 = document.querySelector('.move1');
+const div2 = document.querySelector('.move2');
+const div3 = document.querySelector('.move3');
 
-        var elem = document.querySelector('.wrapper'),
-            div = document.querySelector('.move'),
-            x = 0,
-            y = 0,
-            mousedown = false;
-        div.addEventListener('mousedown', function (e) {
-            mousedown = true;
-            x = div.offsetLeft - e.clientX;
-            y = div.offsetTop - e.clientY;
-        }, true);
-        div.addEventListener('mouseup', function (e) {
-            mousedown = false;
-        }, true);
-        elem.addEventListener('mousemove', function (e) {
-            e.preventDefault();
-            if (mousedown) {
-                div.style.left = e.clientX + x + 'px';
-                div.style.top = e.clientY + y + 'px';
-            }
-        }, true);
-    }
+const moverclass = {
+  init(elem, d) {
+    let x = 0;
+    let y = 0;
+    let mousedown = false;
+    d.addEventListener(
+      'mousedown',
+      function(e) {
+        mousedown = true;
+        x = d.offsetLeft - e.clientX;
+        y = d.offsetTop - e.clientY;
+      },
+      true
+    );
+    d.addEventListener(
+      'mouseup',
+      function(e) {
+        mousedown = false;
+      },
+      true
+    );
+    elem.addEventListener(
+      'mousemove',
+      function(e) {
+        e.preventDefault();
+        if (mousedown) {
+          d.style.left = `${e.clientX + x}px`;
+          d.style.top = `${e.clientY + y}px`;
+        }
+      },
+      true
+    );
+  },
 };
-moverclass.__init();
-var moverclass = {
-    __init: function () {
-        var elem = document.querySelector('.wrapper1'),
-            div = document.querySelector('.move1'),
-            x = 0,
-            y = 0,
-            mousedown = false;
-        div.addEventListener('mousedown', function (e) {
-            mousedown = true;
-            x = div.offsetLeft - e.clientX;
-            y = div.offsetTop - e.clientY;
-        }, true);
-        div.addEventListener('mouseup', function (e) {
-            mousedown = false;
-        }, true);
-        elem.addEventListener('mousemove', function (e) {
-            e.preventDefault();
-            if (mousedown) {
-                div.style.left = e.clientX + x + 'px';
-                div.style.top = e.clientY + y + 'px';
-            }
-        }, true);
-    }
-};
-moverclass.__init();
-var moverclass = {
-    __init: function () {
-        var elem = document.querySelector('.wrapper2'),
-            div = document.querySelector('.move2'),
-            x = 0,
-            y = 0,
-            mousedown = false;
-        div.addEventListener('mousedown', function (e) {
-            mousedown = true;
-            x = div.offsetLeft - e.clientX;
-            y = div.offsetTop - e.clientY;
-        }, true);
-        div.addEventListener('mouseup', function (e) {
-            mousedown = false;
-        }, true);
-        elem.addEventListener('mousemove', function (e) {
-            e.preventDefault();
-            if (mousedown) {
-                div.style.left = e.clientX + x + 'px';
-                div.style.top = e.clientY + y + 'px';
-            }
-        }, true);
-    }
-};
-moverclass.__init();
-var moverclass = {
-    __init: function () {
-        var elem = document.querySelector('.wrapper3'),
-            div = document.querySelector('.move3'),
-            x = 0,
-            y = 0,
-            mousedown = false;
-        div.addEventListener('mousedown', function (e) {
-            mousedown = true;
-            x = div.offsetLeft - e.clientX;
-            y = div.offsetTop - e.clientY;
-        }, true);
-        div.addEventListener('mouseup', function (e) {
-            mousedown = false;
-        }, true);
-        elem.addEventListener('mousemove', function (e) {
-            e.preventDefault();
-            if (mousedown) {
-                div.style.left = e.clientX + x + 'px';
-                div.style.top = e.clientY + y + 'px';
-            }
-        }, true);
-    }
-};
-moverclass.__init();
-
+moverclass.init(wrapper, div);
+moverclass.init(wrapper1, div1);
+moverclass.init(wrapper2, div2);
+moverclass.init(wrapper3, div3);
